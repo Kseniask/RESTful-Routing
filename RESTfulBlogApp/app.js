ba5d1141a690131cd527d2840082574d2cc4cb29
kseniask <kseniask30@icloud.com>
2020-06-20 11:22:20 -0700

    CRUD on posts and design done

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -1,7 +1,9 @@
 var bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   express = require('express'),
-  app = express()
+  methodOverride = require('method-override'),
+  expressSanitizer = require('express-sanitizer')
+app = express()
 
 // APP CONFIG
 mongoose.connect('mongodb://localhost/restful_blog_app')
@@ -9,6 +11,8 @@ app.set('view engine', 'ejs')
 //fo css
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(expressSanitizer())
+app.use(methodOverride('_method'))
 
 var blogSchema = new mongoose.Schema({
   title: String,
@@ -40,6 +44,7 @@ app.get('/blogs', (req, res) => {
 
 //create route
 app.post('/blogs', (req, res) => {
+  req.body.body = req.sanitize(req.body.body)
   Blog.create(
     {
       title: req.body.title,
@@ -66,6 +71,47 @@ app.get('/blogs/:id', (req, res) => {
   })
 })
 
+//EDIT
+
+app.get('/blogs/:id/edit', (req, res) => {
+  Blog.findById(req.params.id, (err, blog) => {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+      res.render('edit', { blogs: blog })
+    }
+  })
+})
+
+//UPDATE ROUTE
+
+app.put('/blogs/:id', (req, res) => {
+  var updatedBlog = {
+    title: req.body.title,
+    image: req.body.image,
+    body: req.body.body
+  }
+  Blog.findByIdAndUpdate(req.params.id, updatedBlog, (err, blog) => {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+      res.redirect('/blogs/' + req.params.id)
+    }
+  })
+})
+
+//DELETE ROUTE
+app.delete('/blogs/:id', (req, res) => {
+  //detroy blog
+  Blog.findByIdAndRemove(req.params.id, err => {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+      res.redirect('/blogs')
+    }
+  })
+  //redirect
+})
 app.listen(3000, function () {
   console.log('SERVER IS RUNNING!')
 })
